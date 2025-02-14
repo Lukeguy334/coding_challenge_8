9e98e1553661848d55a4427cfe49645a8598e2e6
Lukeguy334 <lhs57@usf.edu>
2025-02-14 13:36:24 -0500

    Task 4 - Car Rental Cost Calculation

--- cc_8.js
+++ cc_8.js
@@ -17,11 +17,21 @@ console.log(calculateDiscount(100, 0.2)); // Expected output: "Final Price: $80.
 console.log(calculateDiscount(250, 0.15)); // Expected output: "Final Price: $212.50"
 
 // Task 3
-const calculateSeriveFee = (amount, seriveType) => {
-   let feeRate = serviceType === "Premium" ? 0.15 : serviceType === "Standard" ? 0.10 ; 0.05;
+const calculateServiceFee = (amount, serviceType) => {
+   let feeRate = serviceType === "Premium" ? 0.15 : serviceType === "Standard" ? 0.10 : 0.05;
    let serviceFee = amount * feeRate;
    return `Service Fee: $${serviceFee.toFixed(2)}`;
 };
 console.log(calculateServiceFee(200, "Premium")); // Expected output: "Service Fee: $30.00"
 console.log(calculateServiceFee(500, "Standard")); // Expected output: "Service Fee: $50.00"
+
+// Task 4
+function calculateRentalCost(days, carType, insurance = false) {
+let dailyRate = carType === "Economy" ? 40 : carType === "Standard" ? 60 : 100;
+let totalCost = days * dailyRate + (insurance ? days * 20 : 0);
+return `Total Rental Cost: $${totalCost}`;
+}
+
+console.log(calculateRentalCost(3, "Economy", true)); // Expected output: "Total Rental Cost: $180"
+console.log(calculateRentalCost(5, "Luxury", false)); // Expected output: "Total Rental Cost: $500"
 
